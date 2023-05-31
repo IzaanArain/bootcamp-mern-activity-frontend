193e16fe105780580001dd7829064d8376f8fbac
IzaanArain <izaansaquib@gmail.com>
2023-05-31 08:41:34 +0500

    readme done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -39,7 +39,7 @@ const Login = () => {
       <div className="card bg-dark text-white m-5">
         <div className="row g-0">
           <div className="col-lg-6">
-            <img src={animateImg} className="card-img-top" alt="my image" />
+            <img src={defaultImg} className="card-img-top" alt="my image"style={{height:"100%"}} />
           </div>
           <div className="col-lg-6">
             <div className="card-body">
